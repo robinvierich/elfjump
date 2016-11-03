cedb1f1b66b61cd7bedca02f0ba8c62f434cd0a6
Robin Vierich <robin@uken.com>
2016-11-03 13:18:56 -0400

    change an ornament type

--- docs/scripts/data.js
+++ docs/scripts/data.js
@@ -1,4 +1,4 @@
-var ORNAMENT_DATA = [{"position":{"x":1035,"y":3359.987673343606},"type":1,"final":false},{"position":{"x":1769.5686587068458,"y":3203.7891064531195},"type":2,"final":false},{"position":{"x":2972.3203056446614,"y":2175.490445644244},"type":1,"final":false},{"position":{"x":2240.376167961179,"y":2137.3345393746035},"type":4,"final":false},{"position":{"x":2987.3203056446614,"y":1571.861200105995},"type":2,"final":false},{"position":{"x":3890.7636524758022,"y":2734.227909092369},"type":5,"final":false},{"position":{"x":1611.7987011996993,"y":2292.812642910321},"type":6,"final":false},{"position":{"x":2449.7224565288907,"y":3439.8909631662236},"type":3,"final":false},{"position":{"x":3401.7636524758022,"y":3198.5114221894414},"type":4,"final":false},{"position":{"x":2257.7873931314502,"y":1167.0519708393822},"type":3,"final":false},{"position":{"x":2769.714914899001,"y":872.8365736954022},"type":4,"final":false},{"position":{"x":2590.8668721300073,"y":310.40616606949766},"type":6,"final":true}];
+var ORNAMENT_DATA = [{"position":{"x":1035,"y":3359.987673343606},"type":1,"final":false},{"position":{"x":1769.5686587068458,"y":3203.7891064531195},"type":2,"final":false},{"position":{"x":2972.3203056446614,"y":2175.490445644244},"type":1,"final":false},{"position":{"x":2240.376167961179,"y":2137.3345393746035},"type":4,"final":false},{"position":{"x":2987.3203056446614,"y":1571.861200105995},"type":2,"final":false},{"position":{"x":3890.7636524758022,"y":2734.227909092369},"type":5,"final":false},{"position":{"x":1611.7987011996993,"y":2292.812642910321},"type":6,"final":false},{"position":{"x":2449.7224565288907,"y":3439.8909631662236},"type":3,"final":false},{"position":{"x":3401.7636524758022,"y":3198.5114221894414},"type":4,"final":false},{"position":{"x":2257.7873931314502,"y":1167.0519708393822},"type":3,"final":false},{"position":{"x":2603.7329227234313,"y":241.74660055425898},"type":6,"final":true},{"position":{"x":2771.7329227234313,"y":658.0111451003163},"type":5,"final":false}];
 var CANDY_CANE_DATA = [{"position":{"x":1729.9875841012554,"y":3026.3194856234395},"assetPath":"images/candycane.png"},{"position":{"x":2361.2111223155566,"y":1951.1844928808923},"assetPath":"images/candycane.png"},{"position":{"x":2371.888942340127,"y":1002.1085733199818},"assetPath":"images/candycane.png"}];
 var g_edges = [
 [ORNAMENT_DATA[0], ORNAMENT_DATA[1]],
@@ -10,10 +10,8 @@ var g_edges = [
 [ORNAMENT_DATA[8], ORNAMENT_DATA[5]],
 [ORNAMENT_DATA[6], ORNAMENT_DATA[3]],
 [ORNAMENT_DATA[4], ORNAMENT_DATA[9]],
-[ORNAMENT_DATA[9], ORNAMENT_DATA[10]],
-[ORNAMENT_DATA[10], ORNAMENT_DATA[11]],
 [ORNAMENT_DATA[2], ORNAMENT_DATA[4]],
 [ORNAMENT_DATA[1], ORNAMENT_DATA[6]],
-];
-
-
+[ORNAMENT_DATA[9], ORNAMENT_DATA[11]],
+[ORNAMENT_DATA[11], ORNAMENT_DATA[10]],
+]
